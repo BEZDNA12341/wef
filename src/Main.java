21aad29c5c135764a2c7dd16ae2a24b7d6386a8b
Yusuf Islamov <wysyfimam3@gmail.com>
2021-09-17 20:25:17 +0300

    new commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 100;
+        int bonus = 0;
+        int cashIn = 1001;
+        balance =  balance + cashIn;
+        if (cashIn > 1000)
+
+        {bonus = cashIn / 100;}
+
+
+        else { bonus = 0;
+        }
+        balance = balance + bonus;
+        System.out.printf (" Бонус "+ bonus);
+        System.out.printf(" Балланс "+ balance);
+    }
+
+}
